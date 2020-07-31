a7a7ab4600f51f944d05b9f9010c8a725162e062
sadSack <prbillingsley89@gmail.com>
2020-07-31 11:21:20 -0500

    App functioning up to controller. parseGuess returning as undefined

---
+++ resources/app.js
@@ -0,0 +1,127 @@
+const view = {
+    displayMessage: function(msg) {
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "gridSquare hit");
+    },
+    displayMiss: function(location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "gridSquare miss");
+    }
+};
+
+const model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+            {locations: ["24", "34", "44"], hits: ["", "", ""]},
+            {locations: ["10", "11", "12"], hits: ["", "", ""]}],
+    
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let locations = ship.locations;
+            let index = locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+const controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        function parseGuess(guess) {
+            const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+            if (guess === null || guess.length !== 2) {
+                alert("Oops, please enter a letter and a number on the board.");
+            } else {
+                let firstChar = guess.charAt(0);
+                let row = alphabet.indexOf(firstChar);
+                let column = guess.charAt(1);
+
+                if (isNaN(row) || isNaN(column)) {
+                    alert("Oops, that isn't on the board.");
+                } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+                    alert("Oops, that's off the board!");
+                } else {
+                    return row + column;
+                }
+            }
+            return null;
+        }
+    }
+};
+
+
+
+console.log(controller);
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+
+
+
+
+
+
+
+
+
+// model.fire("53");
+
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+
+
+
+
+
+
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+
+// view.displayMessage("Can you hear me?");
