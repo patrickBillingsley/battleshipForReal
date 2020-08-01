039423bb11bea6b4c40aaa683bcfbf83439932d8
sadSack <prbillingsley89@gmail.com>
2020-08-01 15:20:48 -0500

    Game functional

--- resources/app.js
+++ resources/app.js
@@ -1,27 +1,47 @@
-const view = {
-    displayMessage: function(msg) {
-        const messageArea = document.getElementById("messageArea");
-        messageArea.innerHTML = msg;
-    },
-    displayHit: function(location) {
-        const cell = document.getElementById(location);
-        cell.setAttribute("class", "gridSquare hit");
-    },
-    displayMiss: function(location) {
-        const cell = document.getElementById(location);
-        cell.setAttribute("class", "gridSquare miss");
-    }
-};
-
 const model = {
     boardSize: 7,
     numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [{locations: ["0", "0", "0"], hits: ["", "", ""]},
-            {locations: ["0", "0", "0"], hits: ["", "", ""]},
-            {locations: ["0", "0", "0"], hits: ["", "", ""]}],
+    ships: [
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] }
+    ],
+        
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (ship.hits[index] === "hit") {
+                view.displayMessage("Oops, you already hit that location!");
+                return true;
+            } else if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    },
 
     generateShipLocations: function() {
         let locations;
@@ -31,16 +51,19 @@ const model = {
             } while (this.collision(locations));
             this.ships[i].locations = locations;
         }
+        console.log("Ships array: ");
+        console.log(this.ships);
     },
+
     generateShip: function() {
         const direction = Math.floor(Math.random() * 2);
         let row;
         let col;
         if (direction === 1) {
             row = Math.floor(Math.random() * this.boardSize);
-            col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
         } else {
-            row = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
             col = Math.floor(Math.random() * this.boardSize);
         }
         
@@ -54,44 +77,32 @@ const model = {
         }
         return newShipLocations;
     },
+
     collision: function(locations) {
         for (let i = 0; i < this.numShips; i++) {
             const ship = this.ships[i];
             for (let j = 0; j < locations.length; j++) {
-                if (ship.locations.indexOf(locations[j])) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
                     return true;
                 }
             }
         }
         return false;
+    }
+};
+
+const view = {
+    displayMessage: function(msg) {
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
     },
-    fire: function(guess) {
-        for (let i = 0; i < this.numShips; i++) {
-            let ship = this.ships[i];
-            let locations = ship.locations;
-            let index = locations.indexOf(guess);
-            if (index >= 0) {
-                ship.hits[index] = "hit";
-                view.displayHit(guess);
-                view.displayMessage("HIT!");
-                if (this.isSunk(ship)) {
-                    view.displayMessage("You sank my battleship!");
-                    this.shipsSunk++;
-                }
-                return true;
-            }
-        }
-        view.displayMiss(guess);
-        view.displayMessage("You missed.");
-        return false;
+    displayHit: function(location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "gridSquare hit");
     },
-    isSunk: function(ship) {
-        for (let i = 0; i < this.shipLength; i++) {
-            if (ship.hits[i] !== "hit") {
-                return false;
-            }
-        }
-        return true;
+    displayMiss: function(location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "gridSquare miss");
     }
 };
 
